0a7526d3986bc6adbb8437e5e73ae07f8395a0e0
Kubaryt <kubastarzyk6554@gmail.com>
2025-01-21 14:54:11 +0100

    feat: Created navbar and paragraphs

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 const items = [
   {
-    label: 'Strona główna',
-    to: '/',
-  },
-  {
     label: 'Aktualności',
     to: '/aktualnosci',
   },
@@ -28,10 +24,15 @@ const items = [
 </script>
 
 <template>
-  <div class="w-full h-[10vh] bg-black border-b-1 border-b-gray-600 flex justify-between items-center px-5 sticky">
-    <img alt="logo Trampów" src="assets/img/logo.png" class="h-[7vh] rounded-full">
+  <div class="w-full h-[12vh] bg-black border-b-1 border-b-gray-600 flex justify-between items-center px-5 sticky">
+    <NuxtLink class="flex justify-start items-center gap-5" to="/">
+      <img alt="logo Trampów" src="assets/img/logo.png" class="h-[7vh] w-[7vh] rounded-full">
+      <p class="font-bold text-lg">
+        45 KDH Trampy
+      </p>
+    </NuxtLink>
     <div class="flex gap-7 items-center justify-end">
-      <NuxtLink v-for="link in items" :key="link.label" :to="link.to">
+      <NuxtLink v-for="link in items" :key="link.label" :to="link.to" class="hover:underline">
         {{ link.label }}
       </NuxtLink>
     </div>
